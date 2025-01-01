a5ec57262b06aa05144480776193600f06aaed41
Prasad <ppkhose6@gmail.com>
2025-01-01 19:20:06 +0530

    Including JSX & Babel

--- EP-3 Laying The Foundation/App.js
+++ EP-3 Laying The Foundation/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World !");
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World !");
+
+const jsxHeading = <h1 id="heading">Hello World using JSX !</h1>
+
+// JSX => React.createElement => Object => HTML Element
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
