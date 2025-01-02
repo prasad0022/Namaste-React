4e4542d6cf372ae3590e6ad4ebced6ed05012d76
Prasad <ppkhose6@gmail.com>
2025-01-02 18:15:24 +0530

    React Functional Component

--- EP-3 Laying The Foundation/App.js
+++ EP-3 Laying The Foundation/App.js
@@ -7,6 +7,17 @@ const jsxHeading = <h1 id="heading">Hello World using JSX !</h1>
 
 // JSX => React.createElement => Object => HTML Element
 
+// React Functional Components :
+
+const Title = () => <h1>Namaste React</h1>;
+
+const HeadingComponent = () => {
+    return <>
+        <Title />
+        <h2>React Functional Component</h2>
+    </>
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
