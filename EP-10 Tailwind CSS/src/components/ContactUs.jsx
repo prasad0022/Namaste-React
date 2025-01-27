e116e88bf2986d6eb253244a3f7d408d8f450cb3
Prasad <ppkhose6@gmail.com>
2025-01-27 22:52:55 +0530

    Initiated project EP-10 to explore Tailwind CSS

---
+++ EP-10 Tailwind CSS/src/components/ContactUs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <h1 class="text-3xl text-center mt-10 bg-red-950 text-white">
+      Hello world!
+    </h1>
+  );
+};
+
+export default ContactUs;
